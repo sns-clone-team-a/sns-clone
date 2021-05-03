b183745a7ab0730e32c88c8c68e486322fe6ac07
Reo Nagahara <reock1020@outlook.jp>
2021-05-03 11:11:29 +0900

    new ui

--- boardapp/models.py
+++ boardapp/models.py
@@ -6,14 +6,15 @@ class BoardModel(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.CharField(max_length=50)
-    sns_image = models.ImageField(upload_to='')
+    sns_image = models.ImageField(upload_to='',null=True, blank=True, default=0)
     good = models.IntegerField(null=True, blank=True, default=0)
     readtext = models.TextField(null=True, blank=True, default='initial')
 
-class FollowModel(models.Model):
+class ProfileModel(models.Model):
     author = models.CharField(max_length=50)
-    follow = models.IntegerField(null=True, blank=True, default=0)
-    followtext = models.TextField(null=True, blank=True, default='initial')
-    befollowed = models.IntegerField(null=True, blank=True, default=0)
-    befollowedtext = models.TextField(null=True, blank=True, default='initial')
-    
+    one_thing = models.TextField()
+    header_image = models.ImageField(upload_to='')
+    follow_number = models.IntegerField(null=True, blank=True, default=0)
+    follow_text = models.TextField(null=True, blank=True, default='initial')
+    befollowed_number = models.IntegerField(null=True, blank=True, default=0)
+    befollowed_text = models.TextField(null=True, blank=True, default='initial')
