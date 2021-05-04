dd90f95e700bdb6427a79d3fbf8ec8fa70f2fa8b
Reo Nagahara <reock1020@outlook.jp>
2021-05-05 01:29:16 +0900

    New-UI and create Icon

--- boardapp/urls.py
+++ boardapp/urls.py
@@ -3,7 +3,7 @@ from .views import signupfunc, loginfunc, listfunc,\
 logoutfunc, detailfunc, goodfunc, BoardCreate, profilefunc, BoardDelete, BoardUpdate, followfunc, followpagefunc, ProfileCreate
 urlpatterns = [
 	path('signup/', signupfunc, name='signup'),
-	path('login/', loginfunc, name='login'),
+	path('', loginfunc, name='login'),
 	path('list/', listfunc, name='list'),
 	path('logout/', logoutfunc, name='logout'),
 	path('detail/<int:pk>', detailfunc, name='detail'),
